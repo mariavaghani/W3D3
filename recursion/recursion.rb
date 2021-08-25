ed8a7e017f5edde31ed1298df37cc085928fb78e
spark <spark11491@gmail.com>
2021-08-25 17:39:41 -0400

    subsets

--- recursion/recursion.rb
+++ recursion/recursion.rb
@@ -141,8 +141,8 @@ end
 
 
 
-p merge_sort([4, 2, 1, 3])
-p merge_sort([38, 27, 43, 3, 9, 82, 10])
+# p merge_sort([4, 2, 1, 3])
+# p merge_sort([38, 27, 43, 3, 9, 82, 10])
 
 
 # [4, 2, 1, 3] 
@@ -153,3 +153,21 @@ p merge_sort([38, 27, 43, 3, 9, 82, 10])
 
 # p merge([2, 4], [1, 3])
 # p merge([9,82], [10])
+
+def subsets(arr)
+    #base
+    # p arr
+    return [[]] if arr.empty?
+    # return [[], arr] if arr.length == 1
+    subset = []
+    subset << [arr.pop]
+    subset << arr
+    subset << subsets(arr)
+    subset
+end
+
+
+# p subsets([]) # => [[]]
+# p subsets([1]) # => [[], [1]]
+# p subsets([1, 2]) # => [[], [1], [2], [1, 2]]
+p subsets([1, 2, 3]) # => [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
